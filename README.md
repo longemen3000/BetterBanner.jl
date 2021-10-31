24ed3774e74cbb6313df92913208dcbbc17ea8ae
longemen3000 <longemen3000@gmail.com>
2021-10-31 18:14:26 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-# BetterBanner
+# BetterBanner.jl
 
-This package was done to solve two issues with the start banner:
-1. from 1.6 onwards, there is a new `show` method for  
+## Motivation
+
+This package was done to solve two issues with the start banner (`Base.banner()`):
+1. from 1.6 onwards, there is a new `show` method for sparse matrices, that uses braille unicode to draw the sparsity pattern. despite of that, the julia start banner still uses ascii, and there are striking differences between the official logo and the logo recreated.
+
+2. The current banner overflows easily. I have a terrible programming enviroment (by choice) where i choose to have a long, narrow terminal screen, and the display quality of the banner suffers each time. The fact that i saw some julia users on mobile phones (!!) with the same problem, pushed me over the edge to do this.
+
+Lets see the current situation:
+![original banner, with enouth space](images/original.PNG)
+
+Seems right, but sometimes, i really want to start julia on a narrow terminal:
+![original banner, with not enouth space](images/original_overflow.PNG)
+
+## The Solution
+
+Enter BetterBanner.jl.
+
+The package (or file, this is only one file with no dependencies,wrapped in a package for convenience) replaces the current banner art with one done with braille unicode:
+
+[new banner, with space](images/new.PNG)
+
+it comes with some text overflow protection:
+![new banner, overflow](images/new_overflow1.PNG)
+![new banner, more overflow](images/new_overflow2.PNG)
+![new banner, too much overflow](images/new_overflow3.PNG)
+![new banner, insane overflow](images/new_overflow4.PNG)
+
+## Usage
+
+At the moment, copy BetterBanner.jl and add this to your `.julia//config//startup.jl` file (if the file or directory is not there, create it first):
+
+```julia
+#startup.jl
+include("BetterBanner.jl")
+Base.banner(io=stdout) = BetterBanner.banner(io)
+```
+and that it.
+
+If the package is registered in the future, you can do:
+
+```julia
+#startup.jl
+using BetterBanner
+Base.banner(io=stdout) = BetterBanner.banner(io)
+```
--- src/BetterBanner.jl
+++ src/BetterBanner.jl
@@ -1,5 +1,135 @@
 module BetterBanner
+function banner(io = stdout)
+    GIT_VERSION_INFO = Base.GIT_VERSION_INFO
+    if GIT_VERSION_INFO.tagged_commit
+        commit_string = Base.TAGGED_RELEASE_BANNER
+    elseif isempty(GIT_VERSION_INFO.commit)
+        commit_string = ""
+    else
+        days = Int(floor((ccall(:jl_clock_now, Float64, ()) - GIT_VERSION_INFO.fork_master_timestamp) / (60 * 60 * 24)))
+        days = max(0, days)
+        unit = days == 1 ? "day" : "days"
+        distance = GIT_VERSION_INFO.fork_master_distance
+        commit = GIT_VERSION_INFO.commit_short
 
-# Write your package code here.
+        if distance == 0
+            commit_string = "Commit $(commit) ($(days) $(unit) old master)"
+        else
+            branch = GIT_VERSION_INFO.branch
+            commit_string = "$(branch)/$(commit) (fork: $(distance) commits, $(days) $(unit))"
+        end
+    end
+    empty_str = ""
+    logoplusspace = 26
+    commit_date = isempty(Base.GIT_VERSION_INFO.date_string) ? "" : "($(split(Base.GIT_VERSION_INFO.date_string)[1]))"
+    doc = "Documentation:"
+    doc_link = "https://docs.julialang.org"
+    general_help = """Type "?" for help"""
+    pkg_help = """"]?" for Pkg help."""
+    _,width = displaysize(io)
+    if width >= 67
+        t1 = empty_str
+        t2 = doc * " " * doc_link
+        t3 = empty_str
+        t4 = general_help * ", " * pkg_help
+        t5 = empty_str
+        t6 = "Version $(Base.VERSION) $(commit_date)"
+        t7 = commit_string
+        t8 = empty_str
+        extra = """
+        
+        """
+    elseif 44 <= width <= 67
+        t1 = doc
+        if width >= 52
+            t2 = doc_link
+        else
+            t2 = "docs.julialang.org"
+        end
+        t3 = empty_str
+        t4 = general_help * ","
+        t5 = pkg_help
+        t6 = empty_str
+        version_str = "Ver. $(Base.VERSION) $commit_date"
+        if length(version_str) + logoplusspace < width
+            t7 = version_str
+            t8 = empty_str
+        else
+            t7 = "Ver. $(Base.VERSION)"
+            t8 = commit_date
+        end
+        extra = """
+        
+        $commit_string
+        """
+    else
+        t1 = empty_str
+        t2 = empty_str
+        t3 = empty_str
+        t4 = empty_str
+        t5 = empty_str
+        t6 = empty_str
+        t7 = empty_str
+        t8 = empty_str
+        #expect overflowing, but the logo is unaffected
+        extra = """
+        
+        $doc 
+        $doc_link
 
-end
+        $(general_help), $pkg_help
+
+        Ver. $(Base.VERSION) $commit_date
+        
+        $commit_string
+        """
+    end
+
+    separator = ifelse(44 <= width, " | ",empty_str)
+    tt1 = separator * t1
+    tt2 = separator * t2
+    tt3 = separator * t3
+    tt4 = separator * t4
+    tt5 = separator * t5
+    tt6 = separator * t6
+    tt7 = separator * t7
+    tt8 = separator * t8
+    if get(io, :color, false)
+        c = Base.text_colors
+        tx = c[:normal] # text
+        jl = c[:normal] # julia
+        d1 = c[:bold] * c[:blue]    # first dot
+        d2 = c[:bold] * c[:red]     # second dot
+        d3 = c[:bold] * c[:green]   # third dot
+        d4 = c[:bold] * c[:magenta] # fourth dot
+
+    
+
+    print("""
+
+                    $(d3)⢰⣿⣿⡆$(tx)   $(tt1)
+                    $(d3)⠈⠛⠛⠁$(tx)   $(tt2)
+     $(d1)⢰⣿⣿⡆$(jl)       ⣴⣾ $(d2)⣾⣿⣷ $(d4)⣾⣿⣷$(tx) $(tt3)  
+     $(d1)⠈⠛⠛⠁$(jl)       ⣿⣿ $(d2)⠙⠛⠋ $(d4)⠙⠛⠋$(tx) $(tt4)
+      ⣿⣿ ⣿⣿  ⣿⣿ ⣿⣿ ⣴⣾ ⣾⠟⠛⣿⣷$(tt5)
+      ⣿⣿ ⣿⣿  ⣿⣿ ⣿⣿ ⣿⣿⢀⣴⡾⠋⣿⣿$(tt6)
+      ⣿⣿ ⠙⢿⣶⠞⣿⣿ ⣿⣿ ⣿⣿⠈⠻⣷⣶⡿⣿$(tt7)
+    ⢶⣦⣿⠟                   $(tt8)
+    $(extra)""")
+    else
+        print("""  
+                    ⢰⠉⠉⡆   $(tt1)
+                    ⠈⠒⠒⠁   $(tt2)
+     ⢰⠉⠉⡆       ⣴⣾ ⡎⠉⢱ ⡎⠉⢱ $(tt3)    
+     ⠈⠒⠒⠁       ⣿⣿ ⠑⠒⠊ ⠑⠒⠊ $(tt4)
+      ⣿⣿ ⣿⣿  ⣿⣿ ⣿⣿ ⣴⣾ ⣾⠟⠛⣿⣷$(tt5) 
+      ⣿⣿ ⣿⣿  ⣿⣿ ⣿⣿ ⣿⣿⢀⣴⡾⠋⣿⣿$(tt6) 
+      ⣿⣿ ⠙⢿⣶⠞⣿⣿ ⣿⣿ ⣿⣿⠈⠻⣷⣶⡿⣿$(tt7) 
+    ⢶⣦⣿⠟                   $(tt8)
+    $(extra)""")
+    end
+
+end #module
+
+
+
